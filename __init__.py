d342eb259295db5547664a69b0f977d9be22ee92
Andrew Suter-Morris <andrew@prenav.com>
2025-02-19 18:37:09 -0700

    naming

--- __init__.py
+++ __init__.py
@@ -13,13 +13,14 @@ NODE_CLASS_MAPPINGS = {
     "TryOffFluxFillModelNode": TryOffFluxFillModelNode,
     "TryOffQuantizerNode": TryOffQuantizerNode,
     "TryOffFluxFillPipelineNode": FluxFillPipelineNode,
-    "TryOnOffModelNode": TryOnOffModelNode
+    "TryOnOffModelNode": TryOnOffModelNode,
+    "TryOnOffRunNode": TryOnOffRunNode
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "TryOffModelNode": "TryOff Model Loader [ComfyUI-Flux-TryOff]",
     "TryOffFluxFillModelNode": "FluxFill Model Loader [ComfyUI-Flux-TryOff]",
-    "TryOffRunNode": "Run TryOff Inference [ComfyUI-Flux-TryOff]",
+    "TryOnOffRunNode": "Run TryOff Inference [ComfyUI-Flux-TryOff]",
     "TryOffQuantizerNode": "TryOff Quantizer [ComfyUI-Flux-TryOff]",
     "TryOffFluxFillPipelineNode": "FluxFill Pipeline Loader [ComfyUI-Flux-TryOff]",
     "TryOnOffModelNode": "TryOn or TryOff Model Loader [ComfyUI-Flux-TryOff]"
